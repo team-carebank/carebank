a588d5d30493eab37569904420e81a013932086d
kyungeon <kyungeon@DESKTOP-O42Q2RI>
2021-12-28 09:13:02 +0900

    CHANGE

--- src/main/java/com/yee/carebank/model/dao/testdao.java
+++ src/main/java/com/yee/carebank/model/dao/testdao.java
@@ -1,25 +1,6 @@
 package com.yee.carebank.model.dao;
 
-import java.util.List;
-
 public class testdao {
 	
-	//경언
-//	void create(BoardVO BoardVo) throws Exception {
-//	}
-//	
-//	BoardVO read(int post_no) throws Exception {
-//		return null;
-//	}
-//	
-//	void update(BoardVO BoardVO) throws Exception {
-//	}
-//	
-//	void delete(int post_no) throws Exception {
-//	}
-//	
-//	List<BoardVO> listAll() throws Exception {
-//		return null;
-//	}
-	
+
 }
