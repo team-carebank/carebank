29fecda969df6d2873941f607d078a8a73ffa8ab
Subin Byun <sbyun95@gmail.com>
2022-01-13 01:55:12 +0900

    기능: 회원 기본정보 수정

---
+++ src/main/java/com/yee/carebank/model/dao/MyInfoDao.java
@@ -0,0 +1,28 @@
+package com.yee.carebank.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.yee.carebank.model.dto.UserDto;
+
+@Repository
+public class MyInfoDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	String NAMESPACE = "user.";
+
+	public int myinfoUpdate(UserDto newdto) {
+		int res = 0;
+		
+		try {
+			res = sqlSession.update(NAMESPACE + "infoUpdate", newdto);
+		} catch (Exception e) {
+			System.out.println("[error] : my info update");
+			e.printStackTrace();
+		}
+		return res;
+	}
+}
