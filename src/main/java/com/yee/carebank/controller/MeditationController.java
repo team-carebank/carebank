3a7fd59a2b0772767c10de5086882b29ad1ec491
Subin Byun <sbyun95@gmail.com>
2022-01-17 18:55:51 +0900

    기타: 컨트롤러 연결 수정

--- src/main/java/com/yee/carebank/controller/MeditationController.java
+++ src/main/java/com/yee/carebank/controller/MeditationController.java
@@ -1,5 +1,7 @@
 package com.yee.carebank.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
